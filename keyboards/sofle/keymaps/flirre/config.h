964369d448fb79f4775264c8af29d3b4c56ae24c
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-12-29 22:57:18 +0100

    lower OLED brightness, increase timeout longer tap term

--- keyboards/sofle/keymaps/flirre/config.h
+++ keyboards/sofle/keymaps/flirre/config.h
@@ -5,3 +5,6 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 #define UNICODE_SELECTED_MODES UC_LNX
+#define OLED_BRIGHTNESS 200
+#define OLED_TIMEOUT 300000
+#define TAPPING_TERM 300
