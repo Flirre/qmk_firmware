1c1c6d97942b0e528d910d611c637f615879ce94
Filip Lindahl <filip.g.lindahl@gmail.com>
2021-09-18 22:27:06 +0200

    update Sofle settings

--- keyboards/sofle/keymaps/flirre/config.h
+++ keyboards/sofle/keymaps/flirre/config.h
@@ -4,6 +4,5 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
-#define UNICODE_SELECTED_MODES UC_LNX
 #define OLED_BRIGHTNESS 200
 #define OLED_TIMEOUT 300000
